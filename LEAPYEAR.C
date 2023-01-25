1b97989b3b2f74cc9758d8cfcfd7d4060cd489dc
Nishitha <121636893+Nishitha202@users.noreply.github.com>
2023-01-26 00:01:20 +0530

    Add files via upload

---
+++ LEAPYEAR.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int year;
+clrscr();
+printf("enter a year\n");
+scanf("%d",&year);
+if(year%400==0)
+{
+printf("It is a leap year",year);
+}
+else if(year%4==0)
+{
+printf("It is a leap year",year);
+}
+else{
+printf("It is not a leap year",year);
+}
+getch();
+}
