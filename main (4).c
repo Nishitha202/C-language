2be2e1edcc8681d3a0f933751d7a43386c4e6ea0
Nishitha <121636893+Nishitha202@users.noreply.github.com>
2023-03-06 19:08:25 +0530

    Add files via upload

---
+++ main (4).c
@@ -0,0 +1,21 @@
+//In the program the current year and year of joining the organization is entered through keyboard.If the number of years for which the employee has served the organization is greater than 5 years,then a bonus 50000/- is given to the employee .If the years of service are not greater than 5, then the program display a message// 
+
+
+#include <stdio.h>
+
+int main()
+{
+    int bonus,cy,yoj,yos;
+    printf("Enter the current year and year of joining");
+    scanf("%d %d",&cy,&yoj);
+    yos=yoj-cy;
+    if(yos>5)
+    {
+        bonus=50000;
+        printf("Bonus= Rs. %d\n",bonus);
+    }
+    else{
+        printf("you are not eligible for bonus this year");
+    }
+    return 0;
+}
