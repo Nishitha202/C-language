b7156e13a90152fb447c4576de3e2789e81dab81
Nishitha <121636893+Nishitha202@users.noreply.github.com>
2023-03-29 12:24:21 +0530

    Add files via upload

---
+++ strcmp.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online C Compiler.
+  
+
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+char s1[10],s2[10];
+printf("enter a string");
+gets(s1);
+printf("enter a string");
+gets(s2);
+if(strcmp(s1,s2)==0)
+    return 0;
+
+    else
+    return 1;
+
+return 0;
+}
